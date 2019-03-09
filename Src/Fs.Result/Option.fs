cc42dcaa31908dd56b1f7bfa026b528c2a1b00c1
albert woo <albertwoo_slaveoftime@hotmail.com>
2019-03-09 11:38:34 +0800

    Add simple option

---
+++ Src/Fs.Result/Option.fs
@@ -0,0 +1,12 @@
+﻿namespace global
+
+
+[<AutoOpen>]
+module OptionComputationExpression =
+    type OptionBuilder() =
+        member __.Return(x) = Some x
+        member __.ReturnFrom(x) = x
+        member __.Bind(x, f) = Option.bind f x
+        member __.Delay(f) = f()
+
+    let option = OptionBuilder()
