62a4bb915017d7385521016989142e1da438249c
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2019-10-28 22:33:40 +0800

    Add number compare

---
+++ Src/Fun.Result/Comparison.fs
@@ -0,0 +1,27 @@
+﻿namespace Fun.Result
+
+[<AutoOpen>]
+module Comparison =
+    let (|NumBetween|_|) min max x =
+        if x > min && x < max then Some()
+        else None
+
+    let (|NumBetweenE|_|) min max x =
+        if x >= min && x <= max then Some()
+        else None
+
+    let (|SmallThan|) value x =
+        if x < value then Some()
+        else None
+
+    let (|SmallEqual|) value x =
+        if x <= value then Some()
+        else None
+
+    let (|LargeThan|) value x =
+        if x > value then Some()
+        else None
+
+    let (|LargeEqual|) value x =
+        if x >= value then Some()
+        else None
